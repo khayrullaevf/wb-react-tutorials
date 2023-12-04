529ae58f9ce10b1cf57319d1b119da055c42bd7a
Fazliddin Khayrullaev <fazliddinkhayrullaev4@gmail.com>
2023-12-04 14:18:08 +0500

    lesson 2

---
+++ src/review.js
@@ -0,0 +1,12 @@
+//Birinchi dars
+
+//npx create-react-app
+//package.json ,src,public
+//single page application
+//virtual ReactDOM
+//jsx
+//import
+
+
+//=======================
+//ikkinchi dars
